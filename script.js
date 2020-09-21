8d037d509382bd2f7a665c1f6e60dbb46ff29116
johirr <71547439+johirr@users.noreply.github.com>
2020-09-21 17:51:40 +0600

    home work

---
+++ script.js
@@ -0,0 +1,243 @@
+alert("johir rayhan : Welcome To My Site!")
+
+var a, b, c;
+a = 20;
+b = 15;
+a = 10;
+c = a + b;
+document.write(c);
+document.write("<br>");
+
+//thumble
+let x, y, z;
+x = 70;
+y = 5;
+z = x * y;
+document.write(z);
+document.write("<br>");
+
+//scrin
+const A = 30;
+const B = 20;
+const C = A + B;
+document.write(C);
+document.write("<br>");
+
+//energy
+var X = "Welcome to my site";
+var Y = 250;
+var Z = true;
+var b = ["html", "css", "javacsript"];
+var c = {
+    firstname: "micle",
+    lastname: "stiv"
+};
+document.write(X);
+document.write("<br>");
+document.write(typeof X);
+document.write("<br>");
+document.write(typeof Y);
+document.write("<br>");
+document.write(typeof Z);
+document.write("<br>");
+document.write(typeof b);
+document.write("<br>");
+document.write(typeof c);
+document.write("<br>");
+
+//operatror st
+var f = 5;
+var g = 4;
+f **= g;
+document.write(f);
+document.write("<br>");
+
+//content
+console.log(20 > 30);
+console.log(20 == 30);
+console.log(20 < 30);
+var D, E;
+D = 200;
+E = 4577;
+console.log(D <= E);
+
+var F, G;
+F = 20;
+G = 98;
+if (F < G) {
+    document.write(G + "..is greater than 20");
+    document.write("<br>");
+}
+
+var F = 20;
+if (F > 30) {
+    document.write("False")
+    document.write("<br>");
+} else {
+    document.write("True")
+    document.write("<br>");
+}
+
+//ferthes
+var num = 98;
+if (num >= 80 && num <= 100) {
+    document.write("A+")
+    document.write("<br>")
+} else if (num < 80 && num >= 70) {
+    document.write("A")
+    document.write("<br>")
+} else if (num < 70 && num >= 60) {
+    document.write("A-")
+    document.write("<br>")
+} else if (num < 60 && num >= 50) {
+    document.write("B")
+    document.write("<br>")
+} else if (num < 50 && num >= 33) {
+    document.write("Anyhow pass")
+    document.write("<br>")
+} else if (num < 33) {
+    document.write("Fail")
+    document.write("<br>")
+} else {
+    document.write("invalid")
+    document.write("<br>")
+}
+
+//sweet
+var day = 1;
+switch (day) {
+    case 1:
+        document.write("Saturday")
+        document.write("<br>")
+
+        break;
+    case 2:
+        document.write("Sunday")
+        document.write("<br>")
+        break;
+    case 3:
+        document.write("Monday")
+        document.write("<br>")
+        break;
+    case 4:
+        document.write("Tuesday")
+        document.write("<br>")
+        break;
+    case 5:
+        document.write(" Wednesday")
+        document.write("<br>")
+        break;
+    case 6:
+        document.write("Thursday")
+        document.write("<br>")
+        break;
+    case 7:
+        document.write("Fryday")
+        document.write("<br>")
+        break;
+    default:
+        document.write("invalid")
+        document.write("<br>")
+        break;
+}
+
+var age = 25;
+switch (true) {
+    case (age > 10 && age <= 15):
+        document.write("You are good")
+        document.write("<br>")
+        break;
+    case (age > 16 && age <= 18):
+        document.write("You are better")
+        document.write("<br>")
+        break;
+    case (age > 25):
+        document.write("You are best")
+        document.write("<br>")
+        break;
+    default:
+        document.write("You are not eligible")
+        document.write("<br>")
+        break;
+}
+
+var h=10;
+var i=20;
+if (h<i) {
+    alert("He is greater")
+    document.write("<br>")
+}else{
+    alert("she is graeter")
+    document.write("<br>")
+}
+
+var num=prompt("What is your number?");
+if (num >= 80 && num <= 100) {
+    document.write("A+")
+    document.write("<br>")
+} else if (num < 80 && num >= 70) {
+    document.write("A")
+    document.write("<br>")
+} else if (num < 70 && num >= 60) {
+    document.write("A-")
+    document.write("<br>")
+} else if (num < 60 && num >= 50) {
+    document.write("B")
+    document.write("<br>")
+} else if (num < 50 && num >= 33) {
+    document.write("pass")
+    document.write("<br>")
+} else if (num < 33) {
+    document.write("Fail")
+    document.write("<br>")
+} else {
+    document.write("invalid")
+    document.write("<br>")
+}
+
+//Function
+ function hello(){
+    document.write("welcome! ");
+    document.write("<br>");
+}
+hello();
+hello();
+hello();
+hello();
+hello();
+
+ function sum(j,k) {
+    document.write(j*k);
+    document.write("<br>");
+ }
+sum(5,28);
+sum(2,25);
+sum(5,47);
+sum(4,49);
+sum(3,46);
+
+//welcome
+var l=10;
+ function bar() {
+    document.write(l);
+    document.write("<br>");
+ }
+bar();
+document.write(l);
+document.write("<br>");
+
+//varaity
+ function rab() {
+     var K=20;
+    document.write(K);
+    document.write("<br>");
+ }
+rab();
+document.write(K);
+document.write("<br>");
+
+//keyboard
+ function wel() {
+    document.write("hello everybody");
+    document.write("<br>");
+ }
